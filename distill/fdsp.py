895419cb18b56a3987a9a9f62e1d9bc4b6607c66
Matthew Feng <mattfeng@mit.edu>
2024-02-19 00:09:15 -0500

    half precision training

--- distill/fdsp.py
+++ distill/fdsp.py
@@ -8,34 +8,37 @@ import lightning as L
 from lightning.pytorch.strategies import FSDPStrategy
 from lightning.pytorch.demos import Transformer, WikiText2
 
+import bitsandbytes as bnb
 
 class LanguageModel(L.LightningModule):
     def __init__(self, vocab_size):
         super().__init__()
         self.model = Transformer(
             vocab_size=vocab_size,
-            nlayers=64,
+            nlayers=int(32 * 2),
             nhid=4096,
             ninp=1024,
             nhead=64,
         )
-    
+
     def training_step(self, batch):
         inp, tgt = batch
         output = self.model(inp, tgt)
         loss = F.nll_loss(output, tgt.view(-1))
         self.log("train_loss", loss, prog_bar=True)
         return loss
-    
+
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=0.1)
+        # return torch.optim.Adam(self.parameters(), lr=0.01, eps=1e-4)
+        # return bnb.optim.Adam8bit(self.parameters(), lr=0.01, eps=1e-4)
+        return bnb.optim.Adam8bit(self.parameters(), lr=0.001, eps=1e-4)
 
 
 L.seed_everything(42)
 
 # data
 dataset = WikiText2()
-train_dataloader = DataLoader(dataset, num_workers=39)
+train_dataloader = DataLoader(dataset, batch_size=32, num_workers=39)
 
 # model
 model = LanguageModel(vocab_size=dataset.vocab_size)
@@ -44,6 +47,7 @@ model = LanguageModel(vocab_size=dataset.vocab_size)
 policy = {nn.TransformerEncoderLayer, nn.TransformerDecoderLayer}
 # strategy = FSDPStrategy(auto_wrap_policy=policy)
 # trainer = L.Trainer(accelerator="cuda", devices=2, strategy=strategy)
-trainer = L.Trainer(accelerator="cuda", devices=2, strategy="ddp")
+trainer = L.Trainer(accelerator="cuda", devices=2, strategy="ddp", precision="16-true")
+# trainer = L.Trainer(accelerator="cuda", devices=2, strategy="ddp")
 trainer.fit(model, train_dataloader)
 trainer.print(torch.cuda.memory_summary())
